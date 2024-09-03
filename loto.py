855d106e582d22ff4aa14c00d42e700c479449db
Milan Stojanovic <milanstojanovic998@gmail.com>
2024-09-03 13:32:07 +0200

    beautiful

--- loto.py
+++ loto.py
@@ -1,36 +1,30 @@
 import random
+from collections import Counter
 from typing import Set
 
 
 def generate_n_numbers(n: int) -> frozenset:
-    return frozenset(random.randint(1, 39) for _ in range(n))
+    return frozenset(random.sample(range(1,40), n))
 
 
 def number_of_guessed(winning: frozenset, guessed: frozenset) -> int:
     #print(winning.intersection(guessed))
-    return len(winning.intersection(guessed))
+    return len(winning & guessed)
 
 
 veljino_prorocanstvo = generate_n_numbers(8)
 dobitna_kombinacija = generate_n_numbers(7)
-broj_dobitaka = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-}
 
-for _ in range(10_000_000):
+results = Counter()
+
+NUM_SIMULATIONS = 10_000_000
+
+for _ in range(NUM_SIMULATIONS):
     veljino_prorocanstvo = generate_n_numbers(8)
     dobitna_kombinacija = generate_n_numbers(7)
-    broj_dobitaka[number_of_guessed(dobitna_kombinacija,veljino_prorocanstvo)] += 1
+    results[number_of_guessed(dobitna_kombinacija,veljino_prorocanstvo)] += 1
 
-for key in broj_dobitaka:
-    broj_dobitaka[key] /= 100_000
+broj_dobitaka = {x: results[x] / NUM_SIMULATIONS * 100 for x in range(8)}
 
 print(broj_dobitaka)
 
