a71d75286428615e9892d71b06668b3aa2145558
Milan Stojanovic <milanstojanovic998@gmail.com>
2024-09-03 13:06:45 +0200

    dudli du

--- loto.py
+++ loto.py
@@ -1,20 +1,36 @@
 import random
+from typing import Set
 
 
-def generate_n_numbers(n: int) -> set[int]:
-    return {random.randint(1,39) for _ in range(n+1)}
+def generate_n_numbers(n: int) -> frozenset:
+    return frozenset(random.randint(1, 39) for _ in range(n))
 
-def number_of_guessed(winning: set[int], guessed: set[int]) -> int:
-    print(winning.intersection(guessed))
+
+def number_of_guessed(winning: frozenset, guessed: frozenset) -> int:
+    #print(winning.intersection(guessed))
     return len(winning.intersection(guessed))
-
-
 
 
 veljino_prorocanstvo = generate_n_numbers(8)
 dobitna_kombinacija = generate_n_numbers(7)
-print(veljino_prorocanstvo)
-print(dobitna_kombinacija)
-print(number_of_guessed(dobitna_kombinacija,veljino_prorocanstvo))
+broj_dobitaka = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+}
 
-mapa = {}
+for _ in range(10_000_000):
+    veljino_prorocanstvo = generate_n_numbers(8)
+    dobitna_kombinacija = generate_n_numbers(7)
+    broj_dobitaka[number_of_guessed(dobitna_kombinacija,veljino_prorocanstvo)] += 1
+
+for key in broj_dobitaka:
+    broj_dobitaka[key] /= 100_000
+
+print(broj_dobitaka)
+
