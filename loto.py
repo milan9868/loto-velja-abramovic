aa4036436a1b0dca02f9984c33b6331b189012f8
Milan Stojanovic <milanstojanovic998@gmail.com>
2024-09-03 15:07:00 +0200

    np sucks

--- loto.py
+++ loto.py
@@ -1,7 +1,7 @@
 import random
 from collections import Counter
-from typing import Set
 import numpy as np
+import time
 
 
 def generate_n_numbers(n: int) -> frozenset:
@@ -12,8 +12,7 @@ def number_of_guessed(winning: frozenset, guessed: frozenset) -> int:
     return len(winning & guessed)
 
 
-veljino_prorocanstvo = generate_n_numbers(8)
-dobitna_kombinacija = generate_n_numbers(7)
+start_time = time.time()
 
 results = Counter()
 
@@ -22,9 +21,9 @@ NUM_SIMULATIONS = 10_000_000
 for _ in range(NUM_SIMULATIONS):
     veljino_prorocanstvo = generate_n_numbers(8)
     dobitna_kombinacija = generate_n_numbers(7)
-    results[number_of_guessed(dobitna_kombinacija,veljino_prorocanstvo)] += 1
+    results[number_of_guessed(dobitna_kombinacija, veljino_prorocanstvo)] += 1
 
 broj_dobitaka = {x: results[x] / NUM_SIMULATIONS * 100 for x in range(8)}
 
 print(broj_dobitaka)
-
+print(time.time() - start_time)
